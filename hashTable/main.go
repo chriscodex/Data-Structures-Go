9e17015b62df429fa1c24b80167eb00bc04619e8
Christian <3003html@gmail.com>
2022-08-31 03:33:29 -0500

    hashfunction created

--- hashTable/main.go
+++ hashTable/main.go
@@ -10,3 +10,12 @@ func NewHashTable(size int) HashTable {
 		data: make([][][]string, size),
 	}
 }
+
+// Hash Function
+func (h *HashTable) hashMethod(key string) int {
+	hash := 0
+	for i := 0; i < len(key); i++ {
+		hash = (hash + int(key[i])) % len(h.data)
+	}
+	return hash
+}
