d1ec98d09fd108589abd9bfb911bf5e25b935d9f
Christian <3003html@gmail.com>
2022-09-07 03:34:54 -0500

    constructor of sinlgylist created

--- singlyLinkedList/main.go
+++ singlyLinkedList/main.go
@@ -9,3 +9,14 @@ type SinglyLinkedList struct {
 	head *Node
 	tail *Node
 }
+
+// Singly Linked List constructor
+func NewSinglyLinkedList(data int) *SinglyLinkedList {
+	n := Node{
+		data: data,
+	}
+	return &SinglyLinkedList{
+		head: &n,
+		tail: &n,
+	}
+}
