4b73d734ad4fa59921a7fa6cdac9fc556dd7ad6d
Christian <3003html@gmail.com>
2022-09-07 03:37:40 -0500

    AddNodeSpecify method created

--- singlyLinkedList/main.go
+++ singlyLinkedList/main.go
@@ -48,3 +48,17 @@ func (sll *SinglyLinkedList) AddNodeBeggin(node *Node) {
 	node.next = sll.head
 	sll.head = node
 }
+
+func (sll *SinglyLinkedList) AddNodeSpecify(node *Node, position int) {
+	if position != 0 {
+		pointer := sll.head
+		for i := 0; i < position-1; i++ {
+			pointer = pointer.next
+		}
+		after := pointer.next
+		node.next = after
+		pointer.next = node
+	} else {
+		sll.AddNodeBeggin(node)
+	}
+}
